87289c4c3606748adce1c1336c112a1d046f10ae
yosuke.yamada <yama.yosuke1997@gmail.com>
2021-02-24 13:03:07 +0000

    second update tasklist

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -10,6 +10,10 @@
             <td>{{ $task->id }}</td>
         </tr>
         <tr>
+            <th>ステータス</th>
+            <td>{{ $task->title }}</td>
+        </tr>
+        <tr>
             <th>タスク</th>
             <td>{{ $task->content }}</td>
         </tr>
